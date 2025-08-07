2a470f1e96a8fd41ce0bb48a93c007b873160912
Begotten-Traveler <solomondev002@gmail.com>
2025-08-07 03:31:08 +0100

    chore: other functionalities added

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const onSwitchButton = document.getElementById("on-switch");
 const offSwitchButton = document.getElementById("off-switch");
 let notificationMessage = document.getElementById("notification-message");
 
+function notificationMessageTimeOut(duration = 4000) {
+       setTimeout(() => {
+        notificationMessage.style.display = "none"
+    }, duration);
+}
 
 function updateMessage(isOn) {
     notificationMessage.textContent = isOn
@@ -13,9 +18,13 @@ function updateMessage(isOn) {
 onSwitchButton.addEventListener("click", function() {
     myBulb.src = "assets/pic_bulbon.gif";
         updateMessage(true);
-})
+        notificationMessage.style.display = "block";
+        notificationMessageTimeOut()
+    })
 
 offSwitchButton.addEventListener("click", function() {
     myBulb.src = "assets/pic_bulboff.gif";
     updateMessage(false);
+    notificationMessage.style.display = "block";
+    notificationMessageTimeOut()
 })
